f16a023363f2a5d1d66a56e3cc7084e15a5c5b39
Artur <arthur.khumaryan@softland.ai>
2025-07-21 15:43:04 +0400

    change rest name

--- src/modules/components/PartySection.jsx
+++ src/modules/components/PartySection.jsx
@@ -12,7 +12,7 @@ export const PartySection = () => {
                   <h3 className="wedding-section-time">17։00</h3>
             </div>
             <CivilMarriageCeremony />
-            <h3 className="wedding-section-church">Թայմ Թու Իթ Գարդեն</h3>
+            <h3 className="wedding-section-church">Time to Eat Garden</h3>
             <span className="wedding-section-village">Կոտայքի մարզ, գ․ Գառնի, Ազատամարտիկների 11</span>
             <a href="https://maps.app.goo.gl/qA2PGwtUBCWQ85WK9" target="_blank">Ինչպես հասնել</a>
         </div>
